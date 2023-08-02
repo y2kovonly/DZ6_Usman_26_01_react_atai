37ba78364204e252b0800dd9ca978149be10cf56
y2kovonly <nuridnovm46@gmail.com>
2023-08-02 17:29:57 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Slideshow} from './components/Slide'
+import "./App.css"
+const App = () => {
+  return (
+    <div><Slideshow/></div>
+  )
+}
+
+export default App
